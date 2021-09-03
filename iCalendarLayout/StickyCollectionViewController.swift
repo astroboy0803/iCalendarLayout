22151d1aef01abfa15bc0c95b6c7ef94935f7a6f
黃宇新 <astroboy03@cathaylife.com.tw>
2021-09-03 17:06:22 +0800

    feat: 初始版面

--- iCalendarLayout/StickyCollectionViewController.swift
+++ iCalendarLayout/StickyCollectionViewController.swift
@@ -7,10 +7,37 @@
 
 import UIKit
 
+// reference: https://www.vadimbulavin.com/sticky-grid-collection-view/
 public final class StickyCollectionViewController: UIViewController {
+    
+    private let collectionView: UICollectionView
+    
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        collectionView = .init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout() )
+        super.init(nibName: nil, bundle: nil)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        let edge: CGFloat = 30
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: edge),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0-edge),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: edge),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0-edge),
+        ])
+        collectionView.backgroundColor = .systemYellow
+        view.backgroundColor = .systemBlue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBlue
     }
 }
 
---
+++ iCalendarLayout/StickyCollectionViewLayout.swift
@@ -0,0 +1,12 @@
+//
+//  StickyCollectionViewLayout.swift
+//  iCalendarLayout
+//
+//  Created by i9400506 on 2021/9/3.
+//
+
+import UIKit
+
+internal final class StickyCollectionViewLayout: UICollectionViewFlowLayout {
+    
+}
